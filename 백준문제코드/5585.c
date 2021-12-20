bee460799147661244082a8c157c8ecc8a216ddc
duckddud213 <duckddud213@naver.com>
2021-12-20 16:53:20 +0900

    5585.c

---
+++ 백준문제코드/5585.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, cnt;
+
+    scanf("%d", &n);
+
+    n = 1000 - n;
+    cnt = 0;
+
+    while (n > 0)
+    {
+        if (n >= 500)
+        {
+            n -= 500;
+            cnt++;
+        }
+        else if (n >= 100)
+        {
+            n -= 100;
+            cnt++;
+        }
+        else if (n >= 50)
+        {
+            n -= 50;
+            cnt++;
+        }
+        else if (n >= 10)
+        {
+            n -= 10;
+            cnt++;
+        }
+        else if (n >= 5)
+        {
+            n -= 5;
+            cnt++;
+        }
+        else if (n >= 1)
+        {
+            n -= 1;
+            cnt++;
+        }
+    }
+
+    printf("%d", cnt);
+}
